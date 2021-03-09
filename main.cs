3ab0b33123591659ad44cf0cb653aa97dff0037e
Elizio Martins Cezarino <elizio.martins@hotmail.com>
2021-03-09 20:19:32 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+class Aula51{
+  static void Main(string[] args){
+
+    if(args.Length > 0){
+      Console.WriteLine("Qtd de argumentos {0}",args.Length);
+      for(int i=0; i < args.Length; i++){
+          Console.WriteLine("Argumento {0}:{1}",i,args[i]);
+      }
+    }else{
+      Console.WriteLine("Não foram passados argumentos");
+    }
+
+    
+  }
+}
